9dc73cae262712b01481b62b1d630fbd469f26b7
戴君維 <bill@daijunweide-MacBook-Air.local>
2018-11-14 14:27:21 +0800

    add: Now app will copy team name automatically when navigating to SlackLoginWebViewPage

--- lib/ui/slack_login/slack_login_web_view_page.dart
+++ lib/ui/slack_login/slack_login_web_view_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:sprouter/ui/slack_login/slack_login_bloc.dart';
@@ -14,9 +17,10 @@ class SlackLoginWebViewPage extends StatefulWidget {
 }
 
 class _SlackLoginWebViewPageState extends State<SlackLoginWebViewPage> {
-  static const String _TEAM_ID = "T024ZT2L3";
+  static const String _teamId = "T024ZT2L3";
+  static const String _teamName = "25sprout";
 
-  bool setupUrlChangedListener = false;
+  bool _initialized = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +29,15 @@ class _SlackLoginWebViewPageState extends State<SlackLoginWebViewPage> {
     final String redirectUrl = bloc?.getSlackRedirectUrl();
     final String clientId = bloc?.getSlackClientId();
 
-    if (!setupUrlChangedListener) {
+    /*Widget webView = WebviewScaffold(
+      appBar: AppBar(
+        title: Text("登入 25Sprout Slack"),
+      ),
+      url:
+          "https://slack.com/oauth/authorize?scope=identity.basic,identity.team,identity.email&team=$_teamId&client_id=$clientId&redirect_uri=$redirectUrl",
+    );*/
+
+    if (!_initialized) {
       flutterWebviewPlugin.onUrlChanged.listen((String changedUrl) async {
         if (!changedUrl.startsWith(redirectUrl)) {
           return;
@@ -36,15 +48,59 @@ class _SlackLoginWebViewPageState extends State<SlackLoginWebViewPage> {
 
         Navigator.of(context).pop(token != null);
       });
-      setupUrlChangedListener = true;
     }
-
-    return WebviewScaffold(
-      appBar: AppBar(
-        title: Text("Log in with Slack"),
-      ),
-      url:
-          "https://slack.com/oauth/authorize?scope=identity.basic,identity.team,identity.email&team=$_TEAM_ID&client_id=$clientId&redirect_uri=$redirectUrl",
+    return FutureBuilder(
+      future: _initialized ? Future.value(true) : showCopyTeamNameHint(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Container();
+        }
+        return WebviewScaffold(
+          appBar: AppBar(
+            title: Text("登入 25Sprout Slack"),
+          ),
+          url:
+              "https://slack.com/oauth/authorize?scope=identity.basic,identity.team,identity.email&team=$_teamId&client_id=$clientId&redirect_uri=$redirectUrl",
+        );
+      },
     );
+  }
+
+  Future<bool> showCopyTeamNameHint() {
+    return Future.delayed(
+      Duration(
+        milliseconds: 0,
+      ),
+    ).then((_) async {
+      await showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          Clipboard.setData(new ClipboardData(text: _teamName));
+
+          return Container(
+            color: Colors.grey.shade800,
+            padding: EdgeInsets.all(8.0),
+            child: RichText(
+              text: TextSpan(
+                style: Theme.of(context).accentTextTheme.body2,
+                children: <TextSpan>[
+                  TextSpan(text: "已複製"),
+                  TextSpan(
+                    text: " $_teamName ",
+                    style: Theme.of(context)
+                        .accentTextTheme
+                        .body2
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(text: "至剪貼簿，任意輕觸以繼續"),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+      _initialized = true;
+      return true;
+    });
   }
 }
